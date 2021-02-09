46653f05fd215f47ba070df7356ef12a3732a72e
mclarck <contact.mclarck@gmail.com>
2021-02-09 18:51:58 -0300

    Fix

--- src/services/useUrl/index.ts
+++ src/services/useUrl/index.ts
@@ -2,8 +2,15 @@ import {useRouteMatch} from "react-router-dom"
 
 const useUrl = () => {
     const {url} = useRouteMatch()
-    const getUrl = (path?: string) => `${url}${path || ""}`
-
+    const getUrl = (path?: string) => {
+        let uri = url
+        if (url) {
+            if (url.charAt(url.length - 1) === "/") {
+                uri = url.substring(0, url.length - 1)
+            }
+        }
+        return `${uri}${path || ""}`
+    }
     return {getUrl}
 }
 export default useUrl
